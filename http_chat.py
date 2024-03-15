f3da6fd9281ff3e91e1b686f60fde90480197be4
Harry Phillips <harryjamesphillips@gmail.com>
2024-03-15 19:02:22 +0000

    llama 2 7b and 13b working

--- http_chat.py
+++ http_chat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import re
 import sys
 import time
 import signal
@@ -43,6 +44,18 @@ app = Flask(__name__)
 
 mutex = Lock()
 
+def deEmojify(text):
+    if "\"" in text:
+        text.replace("\"", "")
+        
+    regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
+
 @app.route("/")
 def query():
     prompt = request.args.get('prompt')
@@ -50,8 +63,12 @@ def query():
         return "No prompt specified"
     if prompt.replace(" ", "").lower() == "you":
         return "Please repeat."
+    if "\"" in prompt:
+        prompt.replace("\"", "")
 
     with mutex:
+        # chat_history.reset()
+
         entry = chat_history.append(role='user', msg=prompt)
         embedding, position = chat_history.embed_chat(return_tokens=not model.has_embed)
         reply = model.generate(
@@ -66,12 +83,18 @@ def query():
             temperature=args.temperature,
             top_p=args.top_p,
         )
+        reply.output_text = deEmojify(reply.output_text)
         bot_reply = chat_history.append(role='bot', text='') # placeholder
         for token in reply:
             continue
 
         chat_history.kv_cache = reply.kv_cache
         bot_reply.text = reply.output_text
+        
+        # situation when LLM output format is broken, recall answer after reseting chat history
+        if "#" in reply.output_text or ("User:" or "AI:" or "Bot:") in reply.output_text :
+            chat_history.reset()
+            query()
 
         return reply.output_text.removesuffix("</s>")
 
